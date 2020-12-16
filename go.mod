cb88abe14bcfb91f350d1c967cc66eb3ef6f5382
Xiao <1109038583@qq.com>
2020-12-16 22:19:41 +0800

    feat: 完成协程池的编写（拷贝）以及测试

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/CodeFish-xiao/go-fish-kit
 go 1.15
 
 require (
+	github.com/alitto/pond v1.4.0 // indirect
 	github.com/hashicorp/consul/api v1.8.1
+	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.6.1
 )
---
+++ sync/thread/gpool_test.go
@@ -0,0 +1,54 @@
+package thread
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestGpool(t *testing.T) {
+	// 创建容量为 10 的任务池
+	pool, err := NewPool(10)
+	if err != nil {
+		panic(err)
+	}
+
+	wg := new(sync.WaitGroup)
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		// 创建任务
+		task := &Task{
+			Handler: func(v ...interface{}) {
+				wg.Done()
+				fmt.Println(v)
+			},
+		}
+		// 添加任务函数的参数
+		task.Params = []interface{}{i, i * 2, "hello"}
+		// 将任务放入任务池
+		pool.Put(task)
+	}
+
+	wg.Add(1)
+	// 再创建一个任务
+	pool.Put(&Task{
+		Handler: func(v ...interface{}) {
+			wg.Done()
+			fmt.Println(v)
+		},
+		Params: []interface{}{"hi!"}, // 也可以在创建任务时设置参数
+	})
+
+	wg.Wait()
+
+	// 安全关闭任务池（保证已加入池中的任务被消费完）
+	pool.Close()
+	// 如果任务池已经关闭, Put() 方法会返回 ErrPoolAlreadyClosed 错误
+	err = pool.Put(&Task{
+		Handler: func(v ...interface{}) {},
+	})
+	if err != nil {
+		fmt.Println(err) // print: pool already closed
+	}
+}
